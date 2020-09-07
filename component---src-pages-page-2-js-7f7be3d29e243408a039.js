072f5c52aeb7f5b30dd1e62d0cfe9bc13812f461
shadman48 <peteranthonyknight48@gmail.com>
2020-09-06 17:02:25 -0700

    YAY...hopefully

---
+++ component---src-pages-page-2-js-7f7be3d29e243408a039.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),c=t("Wbzz");n.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-7f7be3d29e243408a039.js.map
---
+++ styles-471fcdfa154728eee930.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{PGGC:function(n,w,o){}}]);
+//# sourceMappingURL=styles-471fcdfa154728eee930.js.map
